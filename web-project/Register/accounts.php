c495dcb3356e95dfc3d4b37c023a0641938ac532
اية محمد <138715179+AyaaMohammedsayed@users.noreply.github.com>
2026-06-29 22:14:22 +0300

    Delete web-project directory

--- web-project/Register/accounts.php
+++
@@ -1,7 +0,0 @@
-<!-- accounts for test website -->
-<?php
-   
-    $accounts=array("ayahame99@gmail.com"=>"123456789101112","habibagabr@gmail.com"=>"123456789101112","bosinasaad@gmail.com"=>"123456789101112");
- 
-   
-?>
